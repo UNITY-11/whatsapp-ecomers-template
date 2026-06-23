05b5a5566d2512b461bb7ce5ab16b7b0b663bb9b
ajmal faris <ajmalfaris.me@gmail.com>
2026-06-23 23:18:24 +0530

    chore(data): update mock product images with Landmarkshops URLs

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "cdn.sanity.io" },
+      { protocol: "https", hostname: "media.landmarkshops.in" },
+      { protocol: "https", hostname: "media-ea.landmarkshops.in" },
     ],
   },
   experimental: {
